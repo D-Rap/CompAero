e8124b7b847f5f7c9b0bfce927d80c1f0ccad7b1
dp16812 <41548066+itsme-smeit@users.noreply.github.com>
2019-11-11 21:20:36 +0000

    Update CompAero.cpp

--- CompAero.cpp
+++ CompAero.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <fstream>
-#define _USE_MATH_DEFINES 
+//#define _USE_MATH_DEFINES 
 #include <cmath>
 #include <vector>
 
@@ -28,9 +28,18 @@ using namespace std;
 int main() {
 	ofstream myfile;
 	myfile.open("coordinates.plt");
-	// float x, y, rx, ry_in, ry, theta, delta_theta, imax, jmax;
-	int j, i, jmax, imax;
+	double rx, ry_in, ry, theta, delta_theta, rin, r, rloop, i, j, jmax, imax;
+	// int i, j, jmax, imax, imax_in;
 	int imax_in;
+	rx = 0.5;
+	cout << "Choose thickness as a percentage of chord (e.g. 1%) : ";
+	cin >> ry_in;
+	cout << "Choose Square Radius (Nfar as a multiplication of chord) :";
+	cin >> rin;
+	r = rin * 2 * rx;
+	rloop = rin * rx;
+	ry = (ry_in / 100) * (2 * rx);
+
 	// ask for imax
 	while (true) {
 		cout << "Choose imax (imax must be divisible by 8): ";
@@ -45,81 +54,118 @@ int main() {
 	}
 	const int imax_array = imax_in + 1;
 
+	// Calculate angle increment if the ellipse
+	delta_theta = (2 * 3.14159265358979323846) / imax;
+
 	// ask for jmax
 	cout << "Choose jmax: ";
 	cin >> jmax;
 
-	double rx, ry_in, ry, theta, delta_theta, rin, r, rloop;
-	// x[imax_array], y[imax_array];
-	rx = 0.5;
-	cout << "Choose thickness as a percentage of chord (e.g. 1%) : ";
-	cin >> ry_in;
-	cout << "Choose Square Radius (Nfar as a multiplication of chord) :";
-	cin >> rin;
-	r = rin*2*rx;
-	rloop = rin * rx;
-	ry = (ry_in / 100) * (2*rx);
-
-	// Calculate angle increment if the ellipse
-	delta_theta = (2 * 3.14159265358979323846) / imax;
-
-	vector<double> x, y;
+	vector<double> x, z;
 
 	theta = 0;
 
-	for (j = 0; j <= imax_in; j++) {
+	for (i = 0; i <= imax_in; i++) {
 		x.push_back(rx*cos(theta));
-		y.push_back(ry*sin(theta));
-		theta = theta + delta_theta;
-		cout << j << ": " << x[j] << ", " << y[j] <<", " << "theta: " << theta << ", \n";
-		myfile << j << ": " << x[j] << ", " << y[j] << ", \n";
+		z.push_back(ry*sin(theta));
+		theta = theta - delta_theta;
+		cout << i << ": " << x[i] << ", " << z[i] <<", " << "theta: " << theta << ", \n";
+		myfile << x[i] << ", " << z[i] << ", \n";
 	}
 
-	myfile.close();
+	/*myfile.close();*/
 
-	ofstream myfile1;
-	myfile1.open("square.plt");
+	//ofstream myfile1;
+	//myfile1.open("square.plt");
 
-	//// find the circumference of the square box and the i increment
+	// find the circumference of the square box and the i increment
 	double circumf, sqr_increment;
 	circumf = 8*r;
 	sqr_increment = circumf / imax_in;
 	// check delta i for clockwise steps
-	cout << imax_in << "i increment is: " << sqr_increment << '\n';
+	cout << imax_in << ": i increment is: " << sqr_increment << '\n';
 
-	vector<double> sqrx, sqry;
+	vector<double> sqrx, sqrz;
 	for (i = 0; i <= (imax / 8); i++) {
 		sqrx.push_back(r);
-		sqry.push_back(0 - sqr_increment * i);
-		cout << i << ' ' << sqrx[i] << ',' << sqry[i] << ", 1 \n"; 
+		sqrz.push_back(0 - sqr_increment * i);
+		cout << i << ' ' << sqrx[i] << ',' << sqrz[i] << ", 1 \n"; 
 	}
 	for (i = (imax / 8)+1; i <= ((imax / 8) * 3); i++) {
-		sqry.push_back(-r);
+		sqrz.push_back(-r);
 		sqrx.push_back((2*r) - (sqr_increment*i));
-		cout << i << ' ' << sqrx[i] << ',' << sqry[i] << ", 2 \n";
+		cout << i << ' ' << sqrx[i] << ',' << sqrz[i] << ", 2 \n";
 	}
 	for (i = ((imax / 8) * 3)+1; i <= ((imax / 8) * 5); i++) {
 		sqrx.push_back(-r);
-		sqry.push_back((sqr_increment * i) - 4 * r);
-		cout << i << ' ' << sqrx[i] << ',' << sqry[i] <<", 3 \n";
+		sqrz.push_back((sqr_increment * i) - 4 * r);
+		cout << i << ' ' << sqrx[i] << ',' << sqrz[i] <<", 3 \n";
 	}
 	for (i = ((imax / 8) * 5)+1; i <= ((imax / 8) * 7); i++) {
-		sqry.push_back(r);
+		sqrz.push_back(r);
 		sqrx.push_back((sqr_increment * i) - 6*r);
-		cout << i << ' ' << sqrx[i] << ',' << sqry[i] <<", 4 \n";
+		cout << i << ' ' << sqrx[i] << ',' << sqrz[i] <<", 4 \n";
 	}
 	for (i = ((imax / 8) * 7)+1; i <= imax; i++) {
 		sqrx.push_back(r);
-		sqry.push_back(8 * r - (sqr_increment * i));
-		cout << i << ' ' << sqrx[i] << ',' << sqry[i] <<", 5 \n";
+		sqrz.push_back(8 * r - (sqr_increment * i));
+		cout << i << ' ' << sqrx[i] << ',' << sqrz[i] <<", 5 \n";
 	}
 
-	//cout << "zeroth element" << sqrx[0] << ", " << sqry[0] << ", \n";
+	// cout << "zeroth element" << sqrx[0] << ", " << sqry[0] << ", \n";
 	for (i = 0; i <= imax_in; i++) {
-		myfile1 << i << sqrx[i] << ", " << sqry[i] << ",\n";
+		myfile << sqrx[i] << ", " << sqrz[i] << endl;
 	}
-	myfile1.close();
-	//return 0;
+	
+	vector<double> deltax, deltaz; 
+
+	for (i = 0; i <= imax_in; i++) {
+		// Going outwards
+		deltax.push_back(sqrx[i] - x[i]);
+		deltaz.push_back(sqrz[i] - z[i]);
+
+		vector<double> xj, zj;
+
+		for (j = 0; j <= jmax - 1; j++) {
+			xj.push_back(x[i] + (pow((j /(jmax - 1)), 2.0) * deltax[i]));
+			zj.push_back(z[i] + (pow((j / (jmax - 1)), 2.0) * deltaz[i]));
+			cout << "outwards " << xj[j] << ", " << zj[j] << endl;
+			myfile << xj[j] << ", " << zj[j] << endl;
+		}
+
+		xj.clear();
+		zj.clear();
+	}
+
+	deltax.clear();
+	deltaz.clear();
+
+	for (j = 0; j <= jmax - 1 ; j++) {
+		// Going clockwise
+		vector<double> xj, zj;
+		double m, mm;
+		m = j / (jmax - 1);
+		mm = pow(m, 2.0);
+
+		for (i = 0; i <= imax_in; i++) {
+			deltax.push_back(mm*(sqrx[i] - x[i]));
+			deltaz.push_back(mm*(sqrz[i] - z[i]));
+
+			xj.push_back(x[i] + (deltax[i]));
+			zj.push_back(z[i] + (deltaz[i]));
+			cout << xj[i] << ", " << zj[i] << endl;
+			myfile << xj[i] << ", " << zj[i] << endl;
+		}
+
+		deltax.clear();
+		deltaz.clear();
+		xj.clear();
+		zj.clear();
+		
+	}
+	
+	myfile.close();
+	return 0;
 }
 
 
